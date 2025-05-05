961fa50a4c1f1b34891a858c9fe7c058005903b3
Mingtaros <leonardow41@gmail.com>
2025-05-05 19:31:43 +0800

    Load from ChromaDB

--- src/ai/langchain_rag.py
+++ src/ai/langchain_rag.py
@@ -6,6 +6,7 @@ from agno.knowledge.pdf_url import PDFUrlKnowledgeBase
 from agno.vectordb.pgvector import PgVector
 from agno.vectordb.chroma import ChromaDb
 from agno.embedder.sentence_transformer import SentenceTransformerEmbedder
+from agno.document.chunking.semantic import SemanticChunking
 
 from dotenv import load_dotenv
 import os
@@ -25,13 +26,13 @@ def create_uud_knowledge_base(pdf_path="documents"):
     )
     uud_kb = PDFKnowledgeBase(
         path=pdf_path,
-        vector_db=vector_db
+        vector_db=vector_db,
     )
     return uud_kb
 
 def create_agent(debug_mode=True):
     uud_kb = create_uud_knowledge_base(pdf_path="documents")
-    # uud_kb.load(recreate=False)
+    uud_kb.load(recreate=False)
     agent = Agent(
         name="law-agent",
         agent_id="law-agent",
@@ -42,11 +43,13 @@ def create_agent(debug_mode=True):
         description="Anda adalah seorang ahli hukum. Anda mengetahui seluk beluk hukum Indonesia dan Undang Undang Dasar yang mendasarinya.",
         instructions=[
             "IMPORTANT: Jawablah dengan bahasa Indonesia yang baik dan benar sesuai dengan Kamus Besar Bahasa Indonesia (KBBI)",
+            "DILARANG Halusinasi. Gunakan dokumen-dokumen yang ada di knowledge-base atau carilah melalui Google.",
             "Pengguna akan memberikan anda suatu kasus yang sedang terjadi. Tolong berikan beberapa hal:",
-            "   1. UUD apa saja kah yang terlanggar. Tolong jelaskan secara singkat.",
+            "   1. Peraturan apa saja kah yang terlanggar. Tolong jelaskan secara singkat.",
             "   2. Cara penanganannya bagaimana.",
             "   3. Hukuman apakah yang pengguna akan dapatkan.",
             "Apabila ada nominal uang yang tersangkut atau terlibat pada kasus ini, tolong berikan dalam mata uang Rupiah Indonesia (IDR)."
+            # "Jawablah pertanyaan pengguna dengan baik dan jujur."
         ],
         knowledge=uud_kb,
         search_knowledge=True,
